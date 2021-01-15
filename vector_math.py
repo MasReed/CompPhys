46c64f9df7ff380893bdfb6ccf01344d42ffeea7
masreed <bluekitdrummer@gmail.com>
2021-01-15 13:34:08 -0600

    added input parsing

--- vector_math.py
+++ vector_math.py
@@ -47,7 +47,6 @@ def abs2(v: list) -> float:
     Returns:
         float: The absolute value of the vector as a float, squared.
     """
-
     return sum([v[i] * v[i] for i in range(len(v))])
 
 
@@ -61,23 +60,52 @@ def normalized(v: list) -> list:
     return [v[i] / sqrt(abs2(v)) for i in range(len(v))]
 
 
-if __name__ == "__main__":
-    a = [1, 2, 3]
-    b = [4, 5, 6]
-    c = [8, 9, 10]
+def parse_input(s: str) -> list:
+    """Parses input string to list.
+    Args:
+        s (str): String input of form '[v1, v2, v3, ..., v_n]'.
+    Returns:
+        list: Vector as list, usable by other functions.
+    """
+    vector_string = s.strip('[').strip(']').split(',')
+    vector_list = [int(s) for s in vector_string]
 
-    # print('[1]: Calculate dot product of two 3D vectors.')
-    # print('[2]: Calculate cross product of two 3D vectors.')
-    # fxn_choice = input()
-    #
-    # if fxn_choice == '1':
-    #     a = input('Enter the first vector as a list. E.g. [1, 2, 3]\n')
-    #     b = input('Enter the second vector as a list. E.g. [4, 5, 6]\n')
-    #     a = int(item) for item in a
-    #     print(a)
-    #     dot_product(a, b)
-    #
-    # elif fxn_choice == '2':
-    #     a = list(input('Enter the first vector as a list. E.g. [1, 2, 3]\n'))
-    #     b = list(input('Enter the second vector as a list. E.g. [4, 5, 6]\n'))
-    #     cross_product(a, b)
+    return vector_list
+
+
+if __name__ == "__main__":
+    # a = [1, 2, 3]
+    # b = [4, 5, 6]
+    # c = [8, 9, 10]
+    # test_input = '[1, 2, 3]'
+
+    input_prompt1 = 'Enter the first vector as a list. E.g. [1, 2, 3]\n'
+    input_prompt2 = 'Enter the second vector as a list. E.g. [4, 5, 6]\n'
+
+    print('[1]: Calculate dot product of two 3D vectors.')
+    print('[2]: Calculate cross product of two 3D vectors.')
+    print('[3]: Calculate the absolute value, squared, of a vector.')
+    print('[4]: Normalize a vector')
+    fxn_choice = input()
+
+    if fxn_choice == '1':
+        a = input(input_prompt1)
+        b = input(input_prompt2)
+        a, b = parse_input(a), parse_input(b)
+        print(dot_product(a, b))
+
+    elif fxn_choice == '2':
+        a = input(input_prompt1)
+        b = input(input_prompt2)
+        a, b = parse_input(a), parse_input(b)
+        print(cross_product(a, b))
+
+    elif fxn_choice == '3':
+        a = input(input_prompt1)
+        a = parse_input(a)
+        print(abs2(a))
+
+    elif fxn_choice == '4':
+        a = input(input_prompt1)
+        a = parse_input(a)
+        print(normalized(a))
