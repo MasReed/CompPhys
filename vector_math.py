f44f0c4ba6f2a2508dd1627d5bda071cc630cba3
masreed <bluekitdrummer@gmail.com>
2021-01-15 13:03:18 -0600

    added abs2, normalized

--- vector_math.py
+++ vector_math.py
@@ -1,4 +1,6 @@
 # A few functions for vector math
+from math import sqrt
+
 
 def dot_product(u: list, v: list) -> float:
     """Calculates the dot product of two numerical vectors represented as lists.
@@ -38,7 +40,44 @@ def cross_product(u: list, v: list) -> list:
     return [i_hat, j_hat, k_hat]
 
 
+def abs2(v: list) -> float:
+    """Calculates the absolute value of a vector, v, squared.
+    Args:
+        v (list): Vector of the form [v1, v2, v3, ..., v_n]
+    Returns:
+        float: The absolute value of the vector as a float, squared.
+    """
+
+    return sum([v[i] * v[i] for i in range(len(v))])
+
+
+def normalized(v: list) -> list:
+    """Normalizes a vector v, such that |v|^2 = 1.
+    Args:
+        v (list): Vector of the form [v1, v2, v3, ..., v_n]
+    Returns:
+        float: The normalized vector.
+    """
+    return [v[i] / sqrt(abs2(v)) for i in range(len(v))]
+
+
 if __name__ == "__main__":
     a = [1, 2, 3]
     b = [4, 5, 6]
     c = [8, 9, 10]
+
+    # print('[1]: Calculate dot product of two 3D vectors.')
+    # print('[2]: Calculate cross product of two 3D vectors.')
+    # fxn_choice = input()
+    #
+    # if fxn_choice == '1':
+    #     a = input('Enter the first vector as a list. E.g. [1, 2, 3]\n')
+    #     b = input('Enter the second vector as a list. E.g. [4, 5, 6]\n')
+    #     a = int(item) for item in a
+    #     print(a)
+    #     dot_product(a, b)
+    #
+    # elif fxn_choice == '2':
+    #     a = list(input('Enter the first vector as a list. E.g. [1, 2, 3]\n'))
+    #     b = list(input('Enter the second vector as a list. E.g. [4, 5, 6]\n'))
+    #     cross_product(a, b)
